912028849eee86f051c3735236c7e19f4e2e2f58
Zaiste <oh@zaiste.net>
2022-02-28 21:24:35 -0800

    add telemetry command suite

---
+++ src/cli/telemetry/disable.ts
@@ -0,0 +1,21 @@
+import type { Arguments, CommandBuilder } from "yargs";
+import { Config } from "../../lib/config.js";
+import _ from "chalk";
+
+export const command = "disable";
+export const desc = "Disable the telemetry";
+
+export const builder: CommandBuilder = (_) => _;
+
+export const handler = async (argv: Arguments) => {
+  console.log(`${_.gray('Saleor Commerce CLI')} · Telemetry\n`)
+
+  await Config.set("telemetry", "false")
+
+  console.log(`${_.bold('Status:')} ${_.red('Disabled')}`)
+
+  console.log(`
+You have opted-out of our ${_.underline('anonymous')} telemetry program. We won't be collecting data from your machine.
+Learn more: ${_.gray('https://saleor.io/')}${_.blueBright('telemetry')}
+  `)
+};
