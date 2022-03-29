798c0029c55cd5e733bd792cc5b8003b9ad461c1
Zaiste <oh@zaiste.net>
2022-03-28 21:53:07 -0700

    make AWS Cognito flow work

--- src/cli/oauth/login.ts
+++ src/cli/oauth/login.ts
@@ -2,22 +2,27 @@ import type { Arguments, CommandBuilder } from "yargs";
 import ora from "ora";
 import { CliUx } from "@oclif/core";
 import { ServerApp, route } from "retes";
-import got from "got";
+import { nanoid } from 'nanoid';
 
 import { delay } from "../../lib/util.js";
 import { Options } from "../../types.js";
+import got from "got";
+import { Config } from "../../lib/config.js";
 
 const { ux: cli } = CliUx;
 const { GET } = route;
 
-const BaseURL = "https://www.oauth.com/playground/auth-dialog.html";
+const ClientID = "5r1kk2fjnfdngoprj7ihlgihku";
+const ClientSecret = "1fh39iiahp0u22b0p9cqt92cd4hs8acc1mat3gv7hacucobc0ff0";
+const RedirectURI = "http://localhost:4000/";
+
+const BaseURL = "https://saleor-cloud-staging-oauth.auth.us-east-1.amazoncognito.com";
 const Params = {
   response_type: "code",
-  client_id: "MLPC6HEUMg03Kyx4JCsvyrqj",
-  redirect_uri: "http://localhost:3001",
-  // redirect_uri: "https://www.oauth.com/playground/authorization-code.html",
-  scope: "photo offline_access",
-  state: "2AH41e6pZmCQp1q0"
+  client_id: ClientID,
+  redirect_uri: RedirectURI,
+  identity_provider: "COGNITO",
+  scope: "phone email profile aws.cognito.signin.user.admin",
 }
 
 export const command = "login";
@@ -26,30 +31,54 @@ export const desc = "Log in to the Saleor Cloud";
 export const builder: CommandBuilder = (_) => _
 
 export const handler = async (argv: Arguments<Options>) => {
-  const generatedState = 'alamakota123';
+  const generatedState = nanoid();
 
-  const spinner = ora('\nLogging in...').start();
-  await delay(1500);
+  // const spinner = ora('\nLogging in...').start();
+  // await delay(1500);
 
   const QueryParams = new URLSearchParams({...Params, state: generatedState });
-  cli.open(`${BaseURL}?${QueryParams}`);
+  const url = `${BaseURL}/login?${QueryParams}`;
+  cli.open(url);
 
   const app = new ServerApp([
-    GET("/", ({ params }) => {
+    GET("/", async ({ params }) => {
       const { state, code } = params;
 
       if (state !== generatedState) {
-        return "Something went wrong"
+        return "Wrong state"
       }
 
-      // access_token
-      const data = got.post('');
+      const Params = {
+        grant_type: "authorization_code",
+        code,
+        client_id: ClientID,
+        redirect_uri: RedirectURI,
+      }
 
-      return "";
+      const auth = Buffer.from(`${ClientID}:${ClientSecret}`).toString('base64');
+
+      try {
+        const data: any = await got.post(`${BaseURL}/oauth2/token`, {
+          form: Params,
+          headers: {
+            Authorization: `Basic ${auth}`
+          }
+        }).json();
+
+        const { access_token, refresh_token } = data;
+
+        await Config.set("token", `Bearer ${access_token}`);
+        await Config.set("refresh_token", refresh_token);
+
+      } catch (error: any) {
+        console.log(error);
+      }
+
+      await app.stop(); 
+      return "Check the CLI";
     })
   ])
-  await app.start(3001);
-
+  await app.start(4000);
 
   spinner.succeed('Success! Access granted and credentials savely stored')
 };
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,118 +1,134 @@
 import chalk from "chalk";
-import Debug from "debug"
+import Debug from "debug";
 import enquirer from "enquirer";
 import Enquirer from "enquirer";
-import got from "got";
+import got, { HTTPError } from "got";
 import { Arguments } from "yargs";
 import { configure } from "../cli/configure.js";
-import { Config } from "../lib/config.js"
+import { Config } from "../lib/config.js";
 import { API, GET } from "../lib/index.js";
-import { AuthError,
-         createProject,
-         promptDatabaseTemplate,
-         promptEnvironment,
-         promptOrganization,
-         promptOrganizationBackup,
-         promptProject,
-         promptSaleorApp,
-         promptVersion, 
-         promptWebhook} from "../lib/util.js";
+import {
+  AuthError,
+  createProject,
+  promptDatabaseTemplate,
+  promptEnvironment,
+  promptOrganization,
+  promptOrganizationBackup,
+  promptProject,
+  promptSaleorApp,
+  promptVersion,
+  promptWebhook,
+} from "../lib/util.js";
 import { CreatePromptResult, Options } from "../types.js";
 
-const debug = Debug('middleware');
+const debug = Debug("middleware");
 
-type Handler = (opts: Options) => (Options | Promise<Options>)
+type Handler = (opts: Options) => Options | Promise<Options>;
 
 export const useToken = async ({ token }: Options) => {
   let opts = {};
 
   if (!token) {
-    const config = await Config.get()
-    debug('useDefault', config)
+    const config = await Config.get();
+    debug("useDefault", config);
     const { token } = config;
 
     if (token) {
-      debug('token read from file')
-      opts = { ...opts, token}
+      debug("token read from file");
+      opts = { ...opts, token };
     } else {
       console.error(chalk.red("Auth token missing"));
-      console.error("Please create token and run " + chalk.green("saleor configure"))
+      console.error(
+        "Please create token and run " + chalk.green("saleor configure")
+      );
 
-      const { runConfig } = await Enquirer.prompt({
-        type: 'confirm',
-        name: 'runConfig',
-        message: 'Would you like to run `saleor configure` now ?'
-      }) as { runConfig: boolean }
+      const { runConfig } = (await Enquirer.prompt({
+        type: "confirm",
+        name: "runConfig",
+        message: "Would you like to run `saleor configure` now ?",
+      })) as { runConfig: boolean };
 
       if (runConfig) {
-        debug('running configure')
-        const newToken = await configure(undefined)
-        opts = { ...opts, token: newToken}
+        debug("running configure");
+        const newToken = await configure(undefined);
+        opts = { ...opts, token: newToken };
       }
     }
   }
 
   return opts;
-}
+};
 
 export const useOrganization = async ({ token, organization }: Options) => {
   let opts = { token, organization };
 
   if (!organization) {
-    const config = await Config.get()
-    debug('useDefault', config)
+    const config = await Config.get();
+    debug("useDefault", config);
     const { organization_slug } = config;
 
     if (organization_slug) {
-      debug('org read from file')
-      opts = { ...opts, ...{ organization: organization_slug } }
+      debug("org read from file");
+      opts = { ...opts, ...{ organization: organization_slug } };
     } else {
       const organization = await promptOrganization(opts);
       await Config.set("organization_slug", organization.value);
       await Config.set("organization_name", organization.name);
-      opts = { ...opts, ...{ organization: organization.value }}
+      opts = { ...opts, ...{ organization: organization.value } };
     }
   }
 
-  console.log(chalk.green("✔"), chalk.bold("Organization ·"), chalk.cyan(opts.organization));
+  console.log(
+    chalk.green("✔"),
+    chalk.bold("Organization ·"),
+    chalk.cyan(opts.organization)
+  );
 
   return opts;
-}
+};
 
-export const useEnvironment = async ({ token, organization, environment }: Options) => {
+export const useEnvironment = async ({
+  token,
+  organization,
+  environment,
+}: Options) => {
   let opts = { token, organization, environment };
 
   if (!environment) {
     const config = await Config.get();
-    debug('useDefault', config);
+    debug("useDefault", config);
     const { environment_id } = config;
 
     if (environment_id) {
-      debug('env read from file')
-      opts = { ...opts, ...{environment: environment_id} }
+      debug("env read from file");
+      opts = { ...opts, ...{ environment: environment_id } };
     } else {
       const environment = await promptEnvironment(opts);
-      opts = { ...opts, ...{environment: environment.value} }
+      opts = { ...opts, ...{ environment: environment.value } };
     }
   }
 
-  console.log(chalk.green("✔"), chalk.bold("Environment ·"), chalk.cyan(opts.environment));
+  console.log(
+    chalk.green("✔"),
+    chalk.bold("Environment ·"),
+    chalk.cyan(opts.environment)
+  );
 
   return opts;
-}
+};
 
 export const interactiveProject = async (argv: Options) => {
   if (!argv.project) {
     const project = await promptProject(argv);
-    if (project.value === undefined ) {
+    if (project.value === undefined) {
       const project = await createProject(argv);
-      return { project: project.value }
+      return { project: project.value };
     }
-    return { project: project.value }
+    return { project: project.value };
   }
 
-  return {}
-}
+  return {};
+};
 
 export const interactiveDatabaseTemplate = async (argv: Options) => {
   if (!argv.database) {
@@ -121,20 +137,20 @@ export const interactiveDatabaseTemplate = async (argv: Options) => {
     return { database: db.value, ...backup };
   }
 
-  if (argv.database === "blank") return { database: null }
-  if (argv.database === "snapshot") return { database: null, restore: true }
+  if (argv.database === "blank") return { database: null };
+  if (argv.database === "snapshot") return { database: null, restore: true };
 
-  return {}
-}
+  return {};
+};
 
 export const interactiveSaleorVersion = async (argv: Options) => {
   if (!argv.saleor) {
     const snapshot = await promptVersion(argv);
-    return { saleor: snapshot.value }
+    return { saleor: snapshot.value };
   }
 
-  return {}
-}
+  return {};
+};
 
 const doLogin = `
 mutation login($email: String!, $password: String!) {
@@ -144,19 +160,19 @@ mutation login($email: String!, $password: String!) {
     refreshToken
   }
 }
-`
+`;
 
 export const interactiveDashboardLogin = async (argv: Options) => {
   if (!argv.email && !argv.password) {
     const { email } = await enquirer.prompt<{ email: string }>({
       type: "text",
-      name: 'email',
-      message: 'Your Saleor Dashboard email?'
+      name: "email",
+      message: "Your Saleor Dashboard email?",
     });
     const { password } = await enquirer.prompt<{ password: string }>({
       type: "password",
-      name: 'password',
-      message: 'Your password?'
+      name: "password",
+      message: "Your password?",
     });
 
     const { domain } = (await GET(API.Environment, argv)) as any;
@@ -165,7 +181,7 @@ export const interactiveDashboardLogin = async (argv: Options) => {
       .post(`https://${domain}/graphql`, {
         json: {
           query: doLogin,
-          variables: { email, password }
+          variables: { email, password },
         },
       })
       .json();
@@ -174,55 +190,65 @@ export const interactiveDashboardLogin = async (argv: Options) => {
       throw new AuthError("cannot login to dashboard");
     }
 
-    const { tokenCreate: { csrfToken, refreshToken } } = data;
-    return { csrfToken, refreshToken }
+    const {
+      tokenCreate: { csrfToken, refreshToken },
+    } = data;
+    return { csrfToken, refreshToken };
   }
 
-  return {}
-}
+  return {};
+};
 
 export const interactiveSaleorApp = async (argv: Options) => {
   if (!argv.app) {
     const app = await promptSaleorApp(argv);
-    return { app: app.value }
+    return { app: app.value };
   }
 
-  return {}
-}
+  return {};
+};
 
 export const interactiveWebhook = async (argv: Options) => {
   if (!argv.webhookID) {
     const webhookID = await promptWebhook(argv);
-    return { webhookID: webhookID.value }
+    return { webhookID: webhookID.value };
   }
 
-  return {}
-}
-
+  return {};
+};
 
 export const useTelemetry = async (argv: Arguments) => {
-  const command = argv._.join(" ")
+  const command = argv._.join(" ");
 
-  const { telemetry } = await Config.get()
-  const isTelemetryEnabled = telemetry === undefined
+  const { telemetry } = await Config.get();
+  const isTelemetryEnabled = telemetry === undefined;
 
-  debug('is telemetry enabled', isTelemetryEnabled);
+  debug("is telemetry enabled", isTelemetryEnabled);
+
 
   if (isTelemetryEnabled) {
-    debug('telemetry', argv._);
-    got.post("https://saleor-cli.deno.dev", {
-      json: { command },
-    });
+    debug("telemetry", argv._);
+
+    try {
+      await got.post("https://saleor-cli.deno.dev", {
+        json: { command },
+      });
+    } catch (error) {
+      if (error instanceof HTTPError) {
+        console.error('Telemetry is broken')
+      }
+      // FIXME
+    }
   }
-}
+};
 
 const checkBackup = async (argv: Options, chosenBackup: CreatePromptResult) => {
   const { name } = chosenBackup;
 
-  if (name === 'snapshot') {
+  if (name === "snapshot") {
     const backup = await promptOrganizationBackup(argv);
-    return { restore_from: backup.value }
+    return { restore_from: backup.value };
   }
 
-  return {}
-}
+  return {};
+};
