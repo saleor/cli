1e02bd62f88ca24026905be86fa295257e6fbfb7
Bartłomiej Wiaduch <tukan2can@gmail.com>
2022-04-13 17:04:41 +0200

    Add webook update to backup restore & env create

--- src/cli/backup/restore.ts
+++ src/cli/backup/restore.ts
@@ -1,8 +1,10 @@
 import type { Arguments, CommandBuilder } from "yargs";
 
-import { API, PUT } from "../../lib/index.js";
+import { API, GET, PUT } from "../../lib/index.js";
 import { Options } from "../../types.js";
 import { promptOrganizationBackup, waitForTask } from "../../lib/util.js";
+import { updateWebhook } from "../webhook/update.js";
+import Enquirer from "enquirer";
 
 export const command = "restore [from]";
 export const desc = "Restore a specific backup";
@@ -29,4 +31,15 @@ export const handler = async (argv: Arguments<Options>) => {
   }) as any;
 
   await waitForTask(argv, result.task_id, 'Restoring', 'Yay! Restore finished!')
+
+  const { update } = await Enquirer.prompt<{ update: string }>({
+    type: "confirm",
+    name: 'update',
+    message: 'Would you like to update webhooks targetUrl',
+  });
+
+  if (update) {
+    const { domain } = await GET(API.Environment, argv) as any;
+    await updateWebhook(domain);
+  }
 };
--- src/cli/webhook/update.ts
+++ src/cli/webhook/update.ts
@@ -15,6 +15,10 @@ export const desc = "Update webhooks for an environment";
 
 export const handler = async (argv: Arguments<Options>) => {
   const { domain } = await GET(API.Environment, argv) as any;
+  await updateWebhook(domain);
+};
+
+export const updateWebhook = async (domain: string) => {
   const gqlUrl = `https://${domain}/graphql`;
   const { token } = await Config.get();
 
@@ -39,13 +43,13 @@ export const handler = async (argv: Arguments<Options>) => {
 
   const { apps: { edges: apps } } = data;
 
-  const { all } = await Enquirer.prompt<{ all: string }>({
+  const { updateAll } = await Enquirer.prompt<{ updateAll: string }>({
     type: "confirm",
-    name: 'all',
-    message: 'Would you like to replace domain for all webhooks',
+    name: 'updateAll',
+    message: 'Would you like to replace domain for all webhooks targetUrl',
   });
 
-  if (all) {
+  if (updateAll) {
     const { webhooksDomain } = await Enquirer.prompt<{ webhooksDomain: string }>({
       type: "input",
       name: 'webhooksDomain',
@@ -59,14 +63,12 @@ export const handler = async (argv: Arguments<Options>) => {
       for (const {id, targetUrl} of webhooks) {
         const url = new URL(targetUrl)
         const newTargetUrl = `${webhooksDomain}${url.pathname}`
-        await updateWebhook(token, gqlUrl, id, newTargetUrl)
+        await runUpdateWebhook(token, gqlUrl, id, newTargetUrl)
       }
     }
 
     spinner.succeed('Yay! Webhooks updated')
-  }
-
-  if (!all) {
+  } else {
     for (const { node: { webhooks, name: appName }} of apps) {
       for (const {id, targetUrl, name} of webhooks) {
         const { newTargetUrl } = await Enquirer.prompt<{ newTargetUrl: string }>({
@@ -77,14 +79,14 @@ export const handler = async (argv: Arguments<Options>) => {
         });
 
         const spinner = ora('Updating...').start();
-        await updateWebhook(token, gqlUrl, id, newTargetUrl)
+        await runUpdateWebhook(token, gqlUrl, id, newTargetUrl)
         spinner.succeed('Updated');
       }
     }
   }
-};
+}
 
-const updateWebhook = async (token:string, gqlUrl: string, id: string, targetUrl: string | null) => {
+const runUpdateWebhook = async (token:string, gqlUrl: string, id: string, targetUrl: string | null) => {
   const { errors }: any = await got.post(gqlUrl, {
     headers: {
       'Authorization-Bearer': token.split(' ').slice(-1),
