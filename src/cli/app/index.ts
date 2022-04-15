ca78c2c325a5a723afd6476bd412941a4dc1eb6b
Zaiste <oh@zaiste.net>
2022-04-15 21:06:09 +0200

    add mgmt for saleor apps

--- src/cli/app/index.ts
+++ src/cli/app/index.ts
@@ -1,13 +1,13 @@
 import * as list from "./list.js";
-// import * as create from "./create.js";
-// import * as show from "./show.js";
+import * as install from "./install.js";
+import * as start from "./start.js";
 import { useEnvironment, useOrganization, useToken } from "../../middleware/index.js";
 
 export default function (_: any) {
   _.command([
     list,
-    // create,
-    // show,
+    install,
+    start,
   ])
   .middleware([useToken, useOrganization, useEnvironment])
   .demandCommand(1, "You need at least one command before moving on");
