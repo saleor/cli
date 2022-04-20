9a23e0edc822f49e7a1b166bc7e3d98e86060525
Zaiste <oh@zaiste.net>
2022-04-20 08:14:38 +0200

    create a template for Saleor App

--- src/cli/app/index.ts
+++ src/cli/app/index.ts
@@ -1,6 +1,7 @@
 import * as list from "./list.js";
 import * as install from "./install.js";
 import * as start from "./start.js";
+import * as create from "./create.js";
 import { useEnvironment, useOrganization, useToken } from "../../middleware/index.js";
 
 export default function (_: any) {
@@ -8,6 +9,7 @@ export default function (_: any) {
     list,
     install,
     start,
+    create,
   ])
   .middleware([useToken, useOrganization, useEnvironment])
   .demandCommand(1, "You need at least one command before moving on");
