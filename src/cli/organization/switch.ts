abaa7cf0d5ea0e1dbf0cba6f2a8302e3a03eedd0
Bartłomiej Wiaduch <tukan2can@gmail.com>
2022-05-09 11:49:53 +0200

    Vaidate organization presence during switch

--- src/cli/organization/switch.ts
+++ src/cli/organization/switch.ts
@@ -2,6 +2,7 @@ import chalk from "chalk";
 import type { Arguments, CommandBuilder } from "yargs";
 
 import { Config } from "../../lib/config.js";
+import { API, GET } from "../../lib/index.js";
 import { promptOrganization } from "../../lib/util.js";
 import { Options } from "../../types.js";
 
@@ -16,10 +17,24 @@ export const builder: CommandBuilder = (_) =>
   });
 
 export const handler = async (argv: Arguments<Options>) => {
-  const organization = argv.slug ? { name: argv.slug, value: argv.slug } :
-                                    await promptOrganization(argv);
+  const organization = await getOrganization(argv);
 
   await Config.set("organization_slug", organization.value);
   await Config.remove("environment_id");
   console.log(chalk.green("✔"), chalk.bold("Organization ·"), chalk.cyan(organization.value));
 };
+
+
+const getOrganization = async (argv: Arguments<Options>) => {
+  if (!argv.slug) {
+    return await promptOrganization(argv);
+  }
+
+  const organizations = await GET(API.Organization, argv) as any[];
+  if (organizations.map(o => o.slug).includes(argv.slug)) {
+    return { name: argv.slug, value: argv.slug }
+  }
+
+  console.warn(chalk.red(`No organization with slug ${argv.slug} found`));
+  process.exit(0);
+}
