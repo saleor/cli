ba3d7c4d9e395887435008ea68620f806abd2f7c
Bartłomiej Wiaduch <tukan2can@gmail.com>
2022-05-09 12:54:30 +0200

    Pass provided slug for org permissions

--- src/cli/organization/permissions.ts
+++ src/cli/organization/permissions.ts
@@ -1,22 +1,26 @@
 import type { Arguments, CommandBuilder } from "yargs";
 
-import { API,  GET } from "../../lib/index.js";
+import { API, GET } from "../../lib/index.js";
 import { showResult } from '../../lib/util.js';
 import { useOrganization } from "../../middleware/index.js";
 import { Options } from "../../types.js";
 
-export const command = "permissions [slug]";
+export const command = "permissions [slug|organization]";
 export const desc = "List organization permissions";
 
 export const builder: CommandBuilder = (_) =>
-  _.positional("organization", {
+  _.positional("slug", {
     type: "string",
     demandOption: false,
     desc: 'slug of the organization'
   });
 
 export const handler = async (argv: Arguments<Options>) => {
-  const result = await GET(API.OrganizationPermissions, {...argv}) as any;
+  const result = await GET(API.OrganizationPermissions,
+    {
+      ...argv,
+      ...{ organization: argv.slug || argv.organization }
+    }) as any;
 
   showResult(result)
 };
