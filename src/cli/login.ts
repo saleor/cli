ec61f87e5e6cfe2097e79e4d0bb0268a07c1707a
Zaiste <oh@zaiste.net>
2022-05-10 20:55:14 +0200

    Send the JWT token for telemetry

--- src/cli/login.ts
+++ src/cli/login.ts
@@ -79,6 +79,7 @@ export const doLogin = async () => {
 
         await Config.reset();
         await Config.set("token", `Token ${token}`);
+        await Config.set("access_token", access_token);
         for (const [name, value] of Object.entries(secrets)) {
           await Config.set(name as ConfigField, value);
         }
