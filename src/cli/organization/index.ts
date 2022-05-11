0b44d80b0e849438acdd0884d994faf9200e0fd5
Bartłomiej Wiaduch <tukan2can@gmail.com>
2022-05-11 11:24:55 +0200

    Drop organization create cmd

--- src/cli/organization/index.ts
+++ src/cli/organization/index.ts
@@ -1,6 +1,5 @@
 import * as show from "./show.js";
 import * as list from "./list.js";
-import * as create from "./create.js";
 import * as remove from "./remove.js";
 import * as permissions from "./permissions.js";
 import * as change from "./switch.js";
@@ -10,7 +9,6 @@ export default function (_: any) {
   _.command([
     show,
     list,
-    create,
     remove,
     permissions,
     change
