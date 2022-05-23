afe8c12b1f9a1ce0723143a6caddb8fdf1e1a50a
Bartłomiej Wiaduch <tukan2can@gmail.com>
2022-05-23 16:45:27 +0200

    Check port availability before vercel login

--- src/lib/detectPort.ts
+++ src/lib/detectPort.ts
@@ -1,11 +1,15 @@
 import detectPort from "detect-port";
 
-export const portIsAvailable = async (
-  port: number
-): Promise<boolean> => {
+export const portIsAvailable = async (port: number): Promise<boolean> => {
   const detectedPort = await detectPort(port).catch((err: Error) => {
     throw new Error(err.message)
   })
 
   return port == detectedPort
+}
+
+export const checkPort = async (port: number): Promise<void> => {
+  if (! await portIsAvailable(port)) {
+    throw new Error(`\nSomething is already running at port 3000\nPlease release port 3000 and try again`)
+  }
 }
