ae4606195fc6bd0bf3b13043c6044b24f7b7153f
Bartłomiej Wiaduch <tukan2can@gmail.com>
2022-05-25 18:40:18 +0200

    Remove downloaded file after extraction

--- src/lib/download.ts
+++ src/lib/download.ts
@@ -9,13 +9,15 @@ const pipeline = promisify(stream.pipeline);
 export const downloadFromGitHub = async (repo: string, dest: string) => {
   const url = `https://github.com/${repo}/archive/master.tar.gz`
   const downloadStream = got.stream(url);
-  const fileWriterStream = fs.createWriteStream('./master.tgz', { mode: 0o755 });
+  const filePath = './master.tgz'
+  const fileWriterStream = fs.createWriteStream(filePath, { mode: 0o755 });
 
   try {
     await pipeline(downloadStream, fileWriterStream);
 
     await fs.ensureDir(dest)
     await tar.x({ file: './master.tgz', cwd: dest, strip: 1 })
+    await fs.remove(filePath)
 
   } catch (error) {
     console.error(`Something went wrong. ${error}`);
