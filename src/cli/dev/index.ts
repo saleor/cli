89d11eedbcec6a9344f4c6dd3c09c6a9daecb05d
Bartłomiej Wiaduch <tukan2can@gmail.com>
2022-07-06 10:06:10 +0200

    Add `dev info` command

--- src/cli/dev/index.ts
+++ src/cli/dev/index.ts
@@ -1,8 +1,9 @@
 import { useGithub } from '../../middleware/index.js';
+import * as info from './info.js';
 import * as prepare from './prepare.js';
 
 export default function (_: any) {
-  _.command([prepare])
+  _.command([prepare, info])
     .middleware([useGithub])
     .demandCommand(1, 'You need at least one command before moving on');
 }
