5744dda6aeb6751eb630f194680fd331b54b0978
Bartłomiej Wiaduch <tukan2can@gmail.com>
2022-10-14 10:31:23 +0200

    Introduce `backup remove` command

--- src/cli/backup/index.ts
+++ src/cli/backup/index.ts
@@ -5,11 +5,12 @@ import {
 } from '../../middleware/index.js';
 import * as create from './create.js';
 import * as list from './list.js';
+import * as remove from './remove.js';
 import * as restore from './restore.js';
 import * as show from './show.js';
 
 export default function (_: any) {
-  _.command([list, create, show, restore])
+  _.command([list, create, show, remove, restore])
     .middleware([useToken, useOrganization, useEnvironment])
     .demandCommand(1, 'You need at least one command before moving on');
 }
