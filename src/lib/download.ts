81c302e89d9eb713c7dc925bc5c7b9b8bf342b74
Bartłomiej Wiaduch <tukan2can@gmail.com>
2022-11-02 13:55:32 +0100

    Introduce `--example` flag for the `app create`

--- src/lib/download.ts
+++ src/lib/download.ts
@@ -1,5 +1,6 @@
 import fs from 'fs-extra';
 import GitURLParse from 'git-url-parse';
+import { join } from 'path';
 import { simpleGit } from 'simple-git';
 
 import { WrongGitURLError } from './util';
@@ -11,6 +12,19 @@ export const gitCopy = async (repo: string, dest: string, ref = 'main') => {
   try {
     const { href: repoURL } = GitURLParse(repo);
     await git.clone(repoURL, dest, { '--branch': ref, '--depth': 1 });
+    await fs.remove(`${dest}/.git`);
+  } catch (error) {
+    throw new WrongGitURLError(`Provided Git URL is invalid: ${repo}`);
+  }
+};
+
+export const gitCopySha = async (repo: string, dest: string, sha: string) => {
+  try {
+    const { href: repoURL } = GitURLParse(repo);
+    await git.clone(repoURL, dest).cwd({ path: dest });
+    const target = join(process.cwd(), dest);
+    await git.cwd({ path: target, root: true });
+    await git.reset(['--hard', sha]);
     await fs.remove(`${dest}/.git`);
   } catch (error) {
     throw new WrongGitURLError(`Provided Git URL is invalid: ${repo}`);
