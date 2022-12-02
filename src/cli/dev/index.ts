0a43b848fbf975fe3714bd3a4987aafac7b4c495
Bartłomiej Wiaduch <tukan2can@gmail.com>
2022-12-02 16:20:05 +0100

    Auto generate docs with `docs` command & husky

--- src/cli/dev/index.ts
+++ src/cli/dev/index.ts
@@ -1,8 +1,9 @@
+import * as docs from './docs.js';
 import * as info from './info.js';
 import * as prepare from './prepare.js';
 
 export default function (_: any) {
-  _.command([prepare, info]).demandCommand(
+  _.command([prepare, info, docs]).demandCommand(
     1,
     'You need at least one command before moving on'
   );
