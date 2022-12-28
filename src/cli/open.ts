d349a27c52d6d21b95aa6c2c8e58602bbbc789bd
Bartłomiej Wiaduch <tukan2can@gmail.com>
2022-12-28 17:21:22 +0100

    Add `open` command
    It opens the browser with docs or instance page

---
+++ src/cli/open.ts
@@ -0,0 +1,62 @@
+import Enquirer from 'enquirer';
+import { Arguments, CommandBuilder } from 'yargs';
+
+import { openURL } from '../lib/util';
+import { useInstanceConnector } from '../middleware';
+import { Options } from '../types';
+
+export const command = 'open [resource]';
+export const desc = 'Open <resource>';
+
+const resources: Record<string, string> = {
+  dashboard: '/dashboard',
+  api: '/graphql/',
+  docs: 'https://docs.saleor.io/docs/3.x/',
+  'docs/api': 'https://docs.saleor.io/docs/3.x/developer',
+  'docs/apps':
+    'https://docs.saleor.io/docs/3.x/developer/extending/apps/key-concepts',
+  'docs/webhooks':
+    'https://docs.saleor.io/docs/3.x/developer/extending/apps/asynchronous-webhooks',
+  'docs/checkout':
+    'https://github.com/saleor/react-storefront/blob/canary/apps/saleor-app-checkout/README.md',
+  'docs/storefront': 'https://github.com/saleor/react-storefront',
+  'docs/cli': 'https://docs.saleor.io/docs/3.x/cli',
+};
+
+export const builder: CommandBuilder = (_) =>
+  _.positional('resource', {
+    type: 'string',
+    demandOption: true,
+    choices: Object.keys(resources),
+  })
+    .example('saleor open dashboard', 'Open instance dashboard')
+    .example('saleor open api', 'Open instance GraphQL endpoint')
+    .example('saleor open docs', 'Open Saleor documentation page');
+
+export const handler = async (argv: Arguments<Options>) => {
+  const choices = Object.keys(resources);
+
+  const { resource } = await Enquirer.prompt<{ resource: string }>({
+    type: 'select',
+    name: 'resource',
+    choices,
+    message: 'Choose the resource',
+    skip: choices.includes(argv.resource as string),
+  });
+
+  const path = resources[resource || (argv.resource as string)];
+
+  if (!path.startsWith('https')) {
+    const url = await getURL(path, argv);
+    await openURL(url);
+    return;
+  }
+
+  await openURL(path);
+};
+
+const getURL = async (path: string, argv: Arguments<Options>) => {
+  const _argv = await useInstanceConnector(argv);
+  const { instance } = _argv;
+  return `${instance}${path}`;
+};
