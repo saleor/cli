6257a726508548a76171edcac537b9375ac9967a
Bartłomiej Wiaduch <tukan2can@gmail.com>
2023-04-28 18:15:33 +0200

    Add prompt and useEnvironment middleware for `saleor env populate`

--- src/cli/env/populate.ts
+++ src/cli/env/populate.ts
@@ -2,13 +2,16 @@ import Debug from 'debug';
 import type { Arguments, CommandBuilder } from 'yargs';
 
 import { API, GET } from '../../lib/index.js';
-import { obfuscateArgv, waitForTask } from '../../lib/util.js';
-import { useBlockingTasksChecker } from '../../middleware/index.js';
+import { confirmRemoval, obfuscateArgv, waitForTask } from '../../lib/util.js';
+import {
+  useBlockingTasksChecker,
+  useEnvironment,
+} from '../../middleware/index.js';
 import { Options } from '../../types.js';
 
 const debug = Debug('saleor-cli:env:populate');
 
-export const command = 'populate <key|environment>';
+export const command = 'populate [key|environment]';
 export const desc = 'Populate database for environment';
 
 export const builder: CommandBuilder = (_) =>
@@ -21,13 +24,23 @@ export const builder: CommandBuilder = (_) =>
 export const handler = async (argv: Arguments<Options>) => {
   debug('command arguments: %O', obfuscateArgv(argv));
 
-  const result = (await GET(API.PopulateDatabase, argv)) as any;
-  await waitForTask(
+  const { environment } = argv;
+
+  const proceed = await confirmRemoval(
     argv,
-    result.task_id,
-    `Populating database: ${argv.environment}`,
-    'Yay! Database populated!'
+    `environment ${environment}`,
+    'replace database with a sample for'
   );
+
+  if (proceed) {
+    const result = (await GET(API.PopulateDatabase, argv)) as any;
+    await waitForTask(
+      argv,
+      result.task_id,
+      `Populating database: ${argv.environment}`,
+      'Yay! Database populated!'
+    );
+  }
 };
 
-export const middlewares = [useBlockingTasksChecker];
+export const middlewares = [useEnvironment, useBlockingTasksChecker];
