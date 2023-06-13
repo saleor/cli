e536f8ef50854bb01273e41039796f19c9b23140
Bartłomiej Wiaduch <tukan2can@gmail.com>
2023-06-13 08:49:18 +0200

    Add tests for `app create` & `app remove`

---
+++ test/functional/app/create.test.ts
@@ -0,0 +1,89 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+
+import {
+  command,
+  DefaultTriggerResponse,
+  prepareEnvironment,
+  testEnvironmentName,
+  testOrganization,
+  trigger,
+} from '../../helper';
+
+const appName = `${testEnvironmentName}-app`;
+
+beforeAll(async () => {
+  await prepareEnvironment();
+}, 1000 * 60 * 10);
+
+afterAll(async () => {
+  await removeApps();
+}, 1000 * 60 * 1);
+
+describe('app create', async () => {
+  it(
+    'should create a Saleor Local App',
+    async () => {
+      const params = [
+        'app',
+        'create',
+        appName,
+        `--environment=${testEnvironmentName}`,
+        `--organization=${testOrganization}`,
+      ];
+      const { exitCode, err } = await trigger(
+        command,
+        params,
+        {},
+        {
+          ...DefaultTriggerResponse,
+          ...{ err: ['App created with id'] },
+        }
+      );
+
+      expect(exitCode).toBe(0);
+      expect(err.join()).toContain('App created with id');
+    },
+    1000 * 60 * 1
+  );
+});
+
+const getApps = async () => {
+  const params = [
+    'app',
+    'list',
+    `--environment=${testEnvironmentName}`,
+    `--organization=${testOrganization}`,
+    '--json',
+  ];
+
+  const { output } = await trigger(
+    command,
+    params,
+    {},
+    {
+      ...DefaultTriggerResponse,
+      ...{ output: ['[]'] },
+    }
+  );
+
+  console.log(output);
+  return JSON.parse(output.join());
+};
+
+const removeApps = async () => {
+  const apps = await getApps();
+
+  for (const app of apps) {
+    const params = [
+      'app',
+      'remove',
+      app.id,
+      `--environment=${testEnvironmentName}`,
+      `--organization=${testOrganization}`,
+      '--force',
+    ];
+
+    console.log(`removing backup ${app.id} - ${app.name}`);
+    await trigger(command, params, {});
+  }
+};
