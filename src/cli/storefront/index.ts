a3117d21ebd4c579dafa28f655ea41e56a4701a9
Bartłomiej Wiaduch <tukan2can@gmail.com>
2023-08-08 13:47:04 +0200

    Use eslint-plugin-import only for ordering

--- src/cli/storefront/index.ts
+++ src/cli/storefront/index.ts
@@ -4,6 +4,6 @@ import * as deploy from './deploy.js';
 export default function (_: any) {
   _.command([create, deploy]).demandCommand(
     1,
-    'You need at least one command before moving on'
+    'You need at least one command before moving on',
   );
 }
