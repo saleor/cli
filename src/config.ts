92f0e757cbefa2fa966167d43e5ac40a67848971
zaiste <oh@zaiste.net>
2023-10-04 12:50:36 +0200

    align the CLI with the new storefront example

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const TelemetryDomain = 'https://telemetry.saleor.io';
 
-export const SaleorStorefrontRepo = 'saleor/react-storefront';
-export const SaleorStorefrontDefaultBranch = 'main';
+export const SaleorStorefrontRepo = 'saleor/storefront';
+export const SaleorStorefrontDefaultBranch = 'canary';
 
 export const SaleorAppDefaultBranch = 'main';
 export const SaleorAppRepo = 'saleor/saleor-app-template';
