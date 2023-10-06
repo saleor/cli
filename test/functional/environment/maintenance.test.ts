0be3b99657988f5ed01d2870b286336862b217b8
Bartłomiej Wiaduch <tukan2can@gmail.com>
2023-10-06 13:09:12 +0200

    Reorganize tests related to env update

---
+++ test/functional/environment/maintenance.test.ts
@@ -0,0 +1,46 @@
+import { describe, expect, it, afterAll } from 'vitest';
+
+import {
+  cleanEnvAfterUpdate,
+  command,
+  DefaultTriggerResponse,
+  getEnvironment,
+  prepareEnvironment,
+  testOrganization,
+  trigger,
+  waitForBlockingTasks,
+  removeEnvironment,
+} from '../../helper';
+
+const envKey = await prepareEnvironment();
+
+afterAll(async () => removeEnvironment(envKey));
+
+describe('update maintenance mode in the environment', async () => {
+  await waitForBlockingTasks(envKey);
+
+  it('`env maintenance` enables the maintenance mode', async () => {
+    const params = [
+      'env',
+      'maintenance',
+      envKey,
+      '--enable',
+      `--organization=${testOrganization}`,
+    ];
+
+    const { exitCode } = await trigger(
+      command,
+      params,
+      {},
+      {
+        ...DefaultTriggerResponse,
+      },
+    );
+    expect(exitCode).toBe(0);
+
+    const environment = await getEnvironment(envKey);
+    expect(environment.maintenance_mode).toBeTruthy();
+  });
+
+  await cleanEnvAfterUpdate(envKey);
+});
